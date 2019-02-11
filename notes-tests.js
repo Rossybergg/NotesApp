98f64cc24988945b6365e0c7db981050f16fc135
Sebastian Lavender <sebastianlavender@outlook.com>
2019-02-11 14:58:49 +0000

    refactered test for include

--- notes-tests.js
+++ notes-tests.js
@@ -1,7 +1,9 @@
-function contains(array, object) {
-  if (array.includes(object)) {
-    return console.log("test passed");
-  } else {
-    return console.log("failed: expcted " + array + " to contain " + object);
+var assert = {
+  contains: function contains(array, object) {
+    if (array.includes(object)) {
+      return console.log("test passed");
+    } else {
+      return console.log("failed: expected " + array + " to contain " + object);
+    }
   }
-};
+}
