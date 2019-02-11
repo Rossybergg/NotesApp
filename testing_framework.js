da0f6338e2ca1dc378cfc879c9d9cef23de64be6
Ross <ross.hunter@tesco.com>
2019-02-11 21:03:02 +0000

    updated test framework console formatting

--- testing_framework.js
+++ testing_framework.js
@@ -2,7 +2,7 @@ var assert = {
 
   contains: function contains(array, object) {
     if (array.includes(object)) {
-      return console.log("%cTest Passed", 'background: #d6ffce; color: #157502; font-size: large');
+      return console.log("%cPASS:", 'background: #d6ffce; color: #157502; font-size: large');
     } else {
       return console.warn(`%cFAIL: expected "${array}" to contain "${object}"`, 'background: #edbbbb; color: #750202; font-size: large');
     }
@@ -11,7 +11,7 @@ var assert = {
   equalsTo: function equalsTo (object1, object2) {
 
     if (object1 === object2) {
-      console.log("%cTest Passed", 'background: #d6ffce; color: #157502; font-size: large');
+      console.log("%cPASS:", 'background: #d6ffce; color: #157502; font-size: large');
     } else {
       console.warn(`%cFAIL: expected "${object1}" to equal "${object2}"`,'background: #edbbbb; color: #750202; font-size: large');
     }
