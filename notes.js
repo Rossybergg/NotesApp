aa2f7aa8d61305b9351b5804a747406e4ed1a38b
Szilvi Horvath <szil.horvath@gmail.com>
2019-02-11 17:02:01 +0000

    test for add notes function implemented

--- notes.js
+++ notes.js
@@ -1,3 +1,13 @@
 function NotesHolder() {
   this.notes = [];
+
+  // function add(string) {
+  //   notes.push(string);
+  // }
+
+
 }
+
+NotesHolder.prototype.add = function(string) {
+  this.notes.push(string);
+};
--- notes_tests.js
+++ notes_tests.js
@@ -1,3 +1,8 @@
 noteholder = new NotesHolder;
 
 describe('list of notes is array', assert.equalsTo(noteholder.notes.length, 0));
+
+noteholder.add("My first note!");
+
+describe('note is pushed into the array',
+assert.contains(noteholder.notes, "My first note!"));
