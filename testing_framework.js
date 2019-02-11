444bb3319f4f72a38eac03dd850fec40601aa2a3
Ross <ross.hunter@tesco.com>
2019-02-11 15:37:59 +0000

    added test pass/fail colour

--- testing_framework.js
+++ testing_framework.js
@@ -2,18 +2,18 @@ var assert = {
 
   contains: function contains(array, object) {
     if (array.includes(object)) {
-      return console.log("Test Passed");
+      return console.log("%c Test Passed", 'color: #00FF00');
     } else {
-      return console.log("FAIL: expected " + array + " to contain " + object);
+      return console.log(`%c FAIL: expected ${array} to contain ${object}`, 'color: #FF0000');
     }
   }
 ,
   equalsTo: function equalsTo (object1, object2) {
 
     if (object1 === object2) {
-      console.log(`Test Passed`);
+      console.log(`%c Test Passed, 'color: #00FF00'`);
     } else {
-      console.log(`FAIL: expected "${object1}" to equal "${object2}"`);
+      console.log(`%c FAIL: expected "${object1}" to equal "${object2}"`,'color: #FF0000');
     }
   }
 
