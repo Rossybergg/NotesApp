21f1f3d4bded10ae8bbbbfce59e73b39276a733b
Ross <ross.hunter@tesco.com>
2019-02-11 15:10:38 +0000

    added equals to test function

--- notes-tests.js
+++ notes-tests.js
@@ -1,9 +1,20 @@
 var assert = {
+
   contains: function contains(array, object) {
     if (array.includes(object)) {
-      return console.log("test passed");
+      return console.log("Test Passed");
     } else {
-      return console.log("failed: expected " + array + " to contain " + object);
+      return console.log("FAIL: expected " + array + " to contain " + object);
     }
   }
+,
+  equalsTo: function equalsTo (object1, object2) {
+
+    if (object1 === object2) {
+      console.log(`Test Passed`);
+    } else {
+      console.log(`FAIL: expected "${object1}" to equal "${object2}"`);
+    }
+  }
+
 }
