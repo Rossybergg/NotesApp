3e79f05b80f2763a2990dff25f86f1064cf3a892
Szilvi Horvath <szil.horvath@gmail.com>
2019-02-12 11:57:55 +0000

    add preview method

--- notes.js
+++ notes.js
@@ -1,13 +1,16 @@
 function NotesHolder() {
-  this.notes = [];
+  var notes = [];
 
-  // function add(string) {
-  //   notes.push(string);
-  // }
+  function preview() {
+    for (i = 0; i <= notes.length; i++) {
+      return notes[i].substr(0, 19);
+    }
+  }
 
+  function add(string) {
+    notes.push(string);
+  }
+
+  return {notes: notes, preview: preview, add: add}
 
 }
-
-NotesHolder.prototype.add = function(string) {
-  this.notes.push(string);
-};
--- notes_tests.js
+++ notes_tests.js
@@ -3,7 +3,10 @@ noteholder = new NotesHolder;
   describe('List of notes is array',
   assert.equalsTo(noteholder.notes.length, 0));
 
-noteholder.add("My first note!");
+noteholder.add("01234567890123456789012345");
 
   describe('Note is pushed into the array',
-  assert.contains(noteholder.notes, "My first note!"));
+  assert.contains(noteholder.notes, "01234567890123456789012345"));
+
+  describe('See the first 20 characters of a note',
+  assert.equalsTo(noteholder.preview(), "0123456789012345678"));
