f0859cdb98af45915bc85cc849ac48b170f34a2e
Ross <ross.hunter@tesco.com>
2019-02-12 20:02:36 +0000

    fixed new note reveal and menu autosizing for messages

--- notes.js
+++ notes.js
@@ -2,11 +2,10 @@ function NotesHolder() {
   var notes = [];
 
   function preview() {
-    text = "<ul>";
+    text = "";
     for (i = 0; i < notes.length; i++) {
-      text += '<a href="">' + notes[i].substr(0, 19) + '</a>';
+      text += '<a href="">' + notes[i].substr(0, 20) + '...</a>';
     }
-    text += "</ul>";
 
     document.getElementById("demo").innerHTML = text;
   }
@@ -34,10 +33,37 @@ function clearTextBox() {
   document.getElementById("newNoteText").value = "";
 }
 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "300px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+function hideNewNote() {
+  var container = document.getElementById("note_id");
+  var image = document.getElementById("image");
+    image.style.display = "block";
+    container.style.display = "none";
+
+}
+
+function showNewNote() {
+  var container = document.getElementById("note_id");
+  var image = document.getElementById("image");
+    image.style.display = "none";
+    container.style.display = "block";
+
+}
+
 // function hide(){
 // var div = document.getElementById('div_id');
 // div.style.visibility = 'hidden';
 // } function that should run after preview (or with show) to make sure the the text entry box dissapears
 
 
-web_noteholder = new NotesHolder
+web_noteholder = new NotesHolder;
+hideNewNote();
