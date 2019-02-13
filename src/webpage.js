5b39c781b73ea071705cd4578ed55b48ce49e077
Szilvi Horvath <szil.horvath@gmail.com>
2019-02-13 11:01:39 +0000

    refactored project file structure

--- src/webpage.js
+++ src/webpage.js
@@ -1,30 +1,3 @@
-function NotesHolder() {
-  var notes = [];
-
-  function preview() {
-    text = "";
-    for (i = 0; i < notes.length; i++) {
-      text += '<a href="">' + notes[i].substr(0, 20) + '...</a>';
-    }
-
-    document.getElementById("demo").innerHTML = text;
-  }
-
-  function add(string) {
-    notes.push(string);
-  }
-
-  function show(string) {
-    for (i = 0; i <= notes.length; i++){
-      if (notes.includes(string))
-      {return notes[i]}
-    }
-  }
-
-  return {notes: notes, preview: preview, add: add, show: show}
-
-}
-
 function getText() {
   return text = document.getElementById("newNoteText").value;
 }
